ae4078a78ae2ef283f11cef7188bbf28901d0fb7
Jan Halsema <janhlsm@gmail.com>
2021-05-07 13:35:41 +0200

    Switch to CPP version of DynRPG

---
+++ DynRPG/include/DynRPG/Utility/Defines.hpp
@@ -0,0 +1,10 @@
+#ifndef RPG_DEFINES_HPP
+#define RPG_DEFINES_HPP
+
+#define LINK_VERSION 3
+
+#ifndef NULL
+	#define NULL 0
+#endif
+
+#endif // RPG_DEFINES_HPP
--- DynRPG/src/EventScriptLine.cpp
+++
@@ -1,9 +0,0 @@
-#define DYNRPG_STATIC
-#include "DynRPG.h"
-
-namespace RPG {
-	int EventScriptLine::parameter(int index) {
-		if(index < 0 || index >= parameters.size) return 0;
-		return parameters[index];
-	}
-}
