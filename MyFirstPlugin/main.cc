1b1df8d9116f32734861c6ecd57b040b89bcc37e
robert <robert@sqrtroot.com>
2021-05-08 01:26:03 +0200

    YES IT WORKS <3

--- MyFirstPlugin/main.cc
+++ MyFirstPlugin/main.cc
@@ -1,25 +1,41 @@
 #include <DynRPG/DynRPG.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 
 // Handler called on startup
-bool onStartup(char *pluginName) {
+bool onStartup(char *pluginName)
+{
     // don't mind this thanks
     // Do not remove or game won't launch.
     volatile short *magic_address = reinterpret_cast<volatile short *>(0x48FA57);
-    *magic_address = 0x9090; 
+    *magic_address = 0x9090;
 
-    if (MessageBox(NULL, // We don't need a window handle
-                   "This is a totally normal RPG Maker game, continue?",                 // Text
-                   "He cometh.", // Title
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    sf::Font font;
+    if (!font.loadFromFile("bahnschrift.ttf"))
+        return false;
+
+    sf::Text text("Hello SFML", font, 50);
+    window.draw(text);
+    window.display();
+
+
+    //
+    if (MessageBox(NULL,                                                 // We don't need a window handle
+                   "This is a totally normal RPG Maker game, continue?", // Text
+                   "He cometh. In anger.",                               // Title
                    MB_YESNO |
                        MB_ICONQUESTION // Flags (yes/no buttons and question icon)
-                   ) == IDYES) {
+                   ) == IDYES)
+    {
         // The user clicked "Yes", we may continue
         return true;
-    } else {
+    }
+    else
+    {
         // The user clicked "No", so we need to abort
         return false;
     }
-
 }
 
 // this doesn't work
