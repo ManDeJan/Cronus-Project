896fe318887c0cda53fa60fb71ef0b491b5e596b
Jan Halsema <janhlsm@gmail.com>
2021-05-07 13:38:10 +0200

    Revert "Revert "Switch to CPP version of DynRPG""
    This reverts commit 9d485637d3da2207072c04b39937045b177fb67a.

--- DynRPG/src/EventScriptData.cpp
+++
@@ -1,9 +0,0 @@
-#define DYNRPG_STATIC
-#include "DynRPG.h"
-
-namespace RPG {
-	EventScriptLine *EventScriptData::line(int index) {
-		if(lines && lines->list) return lines->list->get(index);
-		return NULL;
-	}
-}
