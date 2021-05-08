a96700f4cac8f5770434b27b1d4a70fefa7a7093
Jan Halsema <janhlsm@gmail.com>
2021-05-08 17:59:41 +0200

    clang format yo

---
+++ copy-dlls.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp /usr/i686-w64-mingw32/bin/libstdc++-6.dll Cronus/
+cp /usr/i686-w64-mingw32/bin/libfreetype-6.dll Cronus/
+cp /usr/i686-w64-mingw32/bin/libssp-0.dll Cronus/
+cp /usr/i686-w64-mingw32/bin/libbz2-1.dll Cronus/
+cp /usr/i686-w64-mingw32/bin/libwinpthread-1.dll Cronus/
+cp /usr/i686-w64-mingw32/bin/libgcc_s_dw2-1.dll Cronus/
+cp SFML/build/lib/*.dll Cronus/
