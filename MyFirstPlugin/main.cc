2a751f6b9fdab0ea613cffb23b6238ac0d63d9aa
robert <robert@sqrtroot.com>
2021-05-08 17:14:04 +0200

    Seperation of concerns

--- MyFirstPlugin/main.cc
+++ MyFirstPlugin/main.cc
@@ -1,58 +1,54 @@
 #include <DynRPG/DynRPG.hpp>
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/PrimitiveType.hpp>
 #include <memory>
 #include <fmt/core.h>
-#include <random>
 #include <assert.h>
 
-std::random_device rd;
-std::minstd_rand0 rg(rd());
-std::uniform_int_distribution uint_dist(0, 255);
-class RPG2K3Window
+class RPG2K3Window : public sf::Sprite
 {
 public:
-    sf::RenderWindow window;
-
-    const size_t screenWidth;
-    const size_t screenHeight;
+    static constexpr size_t screenWidth = 320;
+    static constexpr size_t screenHeight = 240;
     std::vector<uint32_t> pixelArray;
     sf::Texture texture;
-    sf::Sprite sprite;
 
-    RPG2K3Window() : window(sf::VideoMode(640, 480), "Cronus"),
-                     screenWidth(RPG::screen->canvas->width()),
-                     screenHeight(RPG::screen->canvas->height()),
-                     pixelArray(screenWidth * screenHeight, 0xFF00FF00),
-                     sprite()
+    RPG2K3Window() : pixelArray(screenWidth * screenHeight, 0xFF00FF00)
     {
-        if (!texture.create(RPG::screen->canvas->width(), RPG::screen->canvas->height()))
+        if (!texture.create(screenWidth, screenHeight))
         {
             throw std::runtime_error("Couldn't generate texture");
         }
-        sprite.setTexture(texture);
-        sprite.setOrigin(screenWidth / 2, screenHeight / 2);
-        sprite.setPosition(640 / 2, 480 / 2);
-        sprite.setScale(640.0 / screenWidth, -1 * (480.0 / screenHeight));
-        fmt::print("Sprite scale: {}x{}", sprite.getScale().x, sprite.getScale().y);
+        sf::Sprite::setTexture(texture);
     }
 
-    void onDraw()
+    void updateTexture()
     {
-        assert(RPG::screen->canvas->getScanline(0) > RPG::screen->canvas->getScanline(screenHeight - 1));
         const uint16_t *buffer = RPG::screen->canvas->getScanline(screenHeight - 1);
-        std::transform(buffer,
-                       buffer + (screenHeight * screenWidth),
-                       pixelArray.begin(),
-                       [](uint16_t input) { return RPG::screen->canvas->convert16To32Bit(input); });
+        // No one look at this jank plz
+        for (int y = screenHeight - 1; y >= 0; y--)
+        {
+            for (int x = 0; x < screenWidth; x++)
+            {
+                pixelArray[x + (screenHeight - 1 - y) * screenWidth] = RPG::screen->canvas->convert16To32Bit(*(buffer + y * screenWidth + x));
+            }
+        }
+
+        // std::transform(buffer,
+        //                buffer + (screenHeight * screenWidth),
+        //                pixelArray.begin(),
+        //                [](uint16_t input) { return RPG::screen->canvas->convert16To32Bit(input); });
 
         texture.update((uint8_t *)pixelArray.data());
-        window.draw(sprite);
-        window.display();
-    };
+    }
+
+    void setTexture(const sf::Texture &texture, bool resetRect = false) = delete;
+    void setTextureRect(const sf::IntRect &rectangle) = delete;
 };
 
-std::unique_ptr<RPG2K3Window> window;
+std::unique_ptr<sf::RenderWindow> window;
+std::unique_ptr<RPG2K3Window> rpg;
 
 // Handler called on startup
 bool onStartup(char *pluginName)
@@ -87,10 +83,13 @@ bool onStartup(char *pluginName)
 
 void onDrawScreen()
 {
-    window->onDraw();
+    rpg->updateTexture();
+    window->draw(*rpg);
+    window->display();
 }
 
 void onInitFinished()
 {
-    window = std::make_unique<RPG2K3Window>();
+    window = std::make_unique<sf::RenderWindow>(sf::VideoMode(640, 480), "Cronus");
+    rpg = std::make_unique<RPG2K3Window>();
 }
