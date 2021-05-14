fabd2ed6252d1ef700a84a101a04012071c732e4
Jan Halsema <janhlsm@gmail.com>
2021-05-14 15:25:35 +0200

    Fix Utilplugin

--- UtilPlugin/main.cc
+++ UtilPlugin/main.cc
@@ -1,14 +1,18 @@
 #include "DynRPG/Character/AnimationType.hpp"
 #include "DynRPG/CommonEvent/CommonEvent.hpp"
+#include "DynRPG/DynCore/PluginCallbacks.hpp"
 #include "DynRPG/Map/Map.hpp"
 #include "DynRPG/Switch/Switches.hpp"
 #include "DynRPG/Terrain/Terrain.hpp"
 #include <DynRPG/DynRPG.hpp>
 #include <fmt/core.h>
 
+#include <ratio>
 #include <string>
 #include <string_view>
 #include <charconv>
+#include <chrono>
+#include <thread>
 
 void change_anim(std::string_view command) {
     fmt::print("Change Anim String: {}\n", command);
@@ -21,9 +25,6 @@ void change_anim(std::string_view command) {
     fmt::print("ints: {}, {}\n", event_id, anim_type);
     // RPG::map->events.get(event_id)->animationType = RPG::AnimationType(anim_type);
     RPG::map->events.get(event_id)->setAnimationType(RPG::AnimationType(anim_type));
-    RPG::map->getLowerLayerTileId(int x, int y);
-    RPG::map->getTerrainId(1)
-    RPG::terrains.get(1)->gridDepth
 }
 
 bool onComment(const char *text, const RPG::ParsedCommentData *parsedData, RPG::EventScriptLine *nextScriptLine, RPG::EventScriptData *scriptData, int eventId, int pageId, int lineId, int *nextLineId) {
@@ -36,6 +37,21 @@ bool onComment(const char *text, const RPG::ParsedCommentData *parsedData, RPG::
 
     return false;
 }
+
+// usefull for measuring frametimes
+// std::chrono::steady_clock::time_point last = std::chrono::steady_clock::now();
+
+// void onDrawScreen() {
+//     std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+//     fmt::print("DrawScreen Time Diff = {:6}μs\n", std::chrono::duration_cast<std::chrono::microseconds>(now - last).count());
+//     last = now;
+// }
+
+// void onFrame(RPG::Scene scene) {
+//     std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+//     fmt::print("OnFrame Time Diff    = {:6}μs\n", std::chrono::duration_cast<std::chrono::microseconds>(now - last).count());
+//     last = now;
+// }
 
 bool onStartup(char *pluginName) {
     fmt::print("Utility Plugin loaded.\n");
